80bf9fe65536b5ec548f6ccdef2dc18f4939038f
Thomas Alpert <thomasalpert@gmail.com>
2022-05-02 15:50:28 +0200

    Update docker-entry.sh

--- server/docker/docker-entry.sh
+++ server/docker/docker-entry.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+# Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
+# See License.txt for license information.
 
-if [ "${1:0:1}" = '-' ]; then
-    set -- mattermost "$@"
-fi
+echo "Starting MySQL"
+/entrypoint.sh mysqld &
 
-exec "$@"
+until mysqladmin -hlocalhost -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" processlist &> /dev/null; do
+        echo "MySQL still not ready, sleeping"
+        sleep 5
+done
 
+echo "Updating CA certificates"
+update-ca-certificates --fresh >/dev/null
+
+echo "Starting platform"
+cd mattermost
+exec ./bin/mattermost --config=config/config_docker.json
